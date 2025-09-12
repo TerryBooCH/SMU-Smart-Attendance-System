b276bde2633b6bfef6f165e0531948831475258d
TerryBooCH <terryboocheeyee@gmail.com>
2025-09-13 02:41:18 +0800

    maven file structure (havent fix pom.xml file)

---
+++ src/main/java/com/smu/smartattendancesystem/SmartattendancesystemApplication.java
@@ -0,0 +1,13 @@
+package com.smu.smartattendancesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartattendancesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartattendancesystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smu/smartattendancesystem/SmartattendancesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smu.smartattendancesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartattendancesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

137eb335406df047194cf21300eda8f4fb4b0cb0
2a22.2309110.lisheng <2a22.2309110.lisheng@gmail.com>
2025-09-19 14:33:13 +0800

    feat: update landing page with new components and styles, add logo, and enhance responsiveness

---
+++ frontend/src/components/PreAuthNavbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Logo from "../assets/logo.svg";
+
+const PreAuthNavbar = () => {
+  return (
+    <header className="lg:flex lg:items-center justify-center fixed top-0 left-0 w-full z-50 backdrop-blur-sm lg:backdrop-blur-sm">
+      <div
+        className={`lg:w-7xl h-17 flex items-center justify-between px-4 lg:px-6`}
+      >
+        <div className="flex items-center justify-start">
+          <a href="/" className={`flex items-center gap-1.5 me-[3rem]`}>
+            <img src={Logo} alt="AgentSoC" className="size-[32px]" />
+
+            <span className={`font-bold text-2xl text-primary `}>Smartend</span>
+          </a>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <a
+            href="/signup"
+            className="text-sm font-medium py-2 px-3 rounded-full border-1 border-[#cecece] text-gray-muted hover:bg-gray-50 hover:shadow-md hover:text-black hover:scale-105 transition-all duration-200"
+          >
+            New Account
+          </a>
+          <a
+            href="/signin"
+            className="text-sm py-2 px-3 bg-primary text-white rounded-full font-medium hover:scale-105 hover:shadow-md transition-all duration-200"
+          >
+            Sign In
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default PreAuthNavbar;

24cca02012fd76791ee8e6d5a8be48374496cbfa
2a22.2309110.lisheng <2a22.2309110.lisheng@gmail.com>
2025-09-19 15:31:00 +0800

    feat: add SignIn page and form with navigation and styling

---
+++ frontend/src/pages/SignIn/SignInForm.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SignInForm = () => {
+  return (
+    <div className="relaive">
+      <div className="min-h-screen py-20 px-4 flex flex-col items-center justify-center">
+        <div className="w-full max-w-md">
+          {/* Header */}
+          <div className="mb-6">
+            <h1 className="font-medium text-3xl sm:text-4xl text-center">
+              Welcome Back
+            </h1>
+          </div>
+          {/* From */}
+          <form action="">
+            <div class="mb-6">
+              <label for="username" class="block mb-2 text-sm font-medium">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                class="bg-gray-50 border border-[#cecece] text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 py-3  "
+                placeholder="Username"
+                required
+              />
+            </div>
+            {/* Submit Button */}
+            <div>
+              <button className="text-white bg-primary font-medium w-full px-2.5 py-2 rounded-xl cursor-pointer">
+                <span>Sign In</span>
+              </button>
+            </div>
+          </form>
+
+          {/* Bottom Link */}
+          <div className="text-center text-sm mt-6">
+            <p>
+              Don't have an account?{" "}
+              <Link
+                to="/signup"
+                className="text-primary transition-colors"
+              >
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignInForm;

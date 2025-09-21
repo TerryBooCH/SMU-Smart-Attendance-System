23f1c16643fb42db4adea82a4ddab5a8f2fd4df9
2a22.2309110.lisheng <2a22.2309110.lisheng@gmail.com>
2025-09-21 18:52:07 +0800

    Added all the pages needed for the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import SignIn from "./pages/SignIn";
+import Students from "./pages/Students";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/students" element={<Students />} />
       </Routes>
     </BrowserRouter>
   );

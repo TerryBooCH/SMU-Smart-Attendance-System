84f455bdd14c8212209e204440b6d63b987de142
P2302676.Xavier Lim <2b03.2302676.xavierlim@gmail.com>
2025-09-21 13:37:01 +0800

    Updated Student class to inherite from BaseEntity class

--- backend/src/main/java/com/smu/smartattendancesystem/models/Student.java
+++ backend/src/main/java/com/smu/smartattendancesystem/models/Student.java
@@ -1,43 +1,69 @@
 package com.smu.smartattendancesystem.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
-public class Student {
+@Table(name = "student")
+public class Student extends BaseEntity { 
 
-    @Id
-    private String studentId; // Example: S12345
+    @Column(nullable = false, unique = true)
+    private String studentId;
 
     private String name;
-    private String classGroup; // Example: CS102
     private String email;
     private String phone;
+
+    // One-to-one with FaceData
+    @OneToOne(mappedBy = "student", cascade = CascadeType.ALL, fetch = FetchType.LAZY) // Any operation on Student also applies to FaceData
+    private FaceData faceData;
 
     // Constructors
     public Student() {}
 
-    public Student(String studentId, String name, String classGroup, String email, String phone) {
+    public Student(String studentId, String name, String email, String phone) {
         this.studentId = studentId;
         this.name = name;
-        this.classGroup = classGroup;
         this.email = email;
         this.phone = phone;
     }
 
-    // Getters and Setters
-    public String getStudentId() { return studentId; }
-    public void setStudentId(String studentId) { this.studentId = studentId; }
+    // Getters & Setters
+    public String getStudentId() { 
+        return studentId; 
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setStudentId(String studentId) { 
+        this.studentId = studentId; 
+    }
 
-    public String getClassGroup() { return classGroup; }
-    public void setClassGroup(String classGroup) { this.classGroup = classGroup; }
+    public String getName() { 
+        return name; 
+    
+    }
+    public void setName(String name) { 
+        this.name = name; 
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { 
+        return email; 
+    }
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
 
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public String getPhone() { 
+        return phone; 
+    }
+
+    public void setPhone(String phone) { 
+        this.phone = phone; 
+    }
+
+    public FaceData getFaceData() { 
+        return faceData; 
+    }
+
+    public void setFaceData(FaceData faceData) { 
+        this.faceData = faceData; 
+    }
 }

e2b87bcdfc5c56f3f3a522c6b4793c64dc6498e5
P2302676.Xavier Lim <2b03.2302676.xavierlim@gmail.com>
2025-09-21 20:05:25 +0800

    Created Session class which inherites from BaseEntity class

---
+++ backend/src/main/java/com/smu/smartattendancesystem/models/Session.java
@@ -0,0 +1,77 @@
+package com.smu.smartattendancesystem.models;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "session")
+public class Session extends BaseEntity {
+
+    private String name;
+    private String startAt;
+    private String endAt;
+    private boolean isOpen;
+    private Integer lateAfterMinutes;
+
+    @OneToMany(mappedBy = "session", cascade = CascadeType.ALL, fetch = FetchType.LAZY) // One-to-many relationship with Attendance
+    private List<Attendance> attendances;
+
+    public Session() {}
+
+    public Session(String name, String startAt, String endAt, boolean isOpen, Integer lateAfterMinutes) { // Constructor to make new sessions
+        this.name = name;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.isOpen = isOpen;
+        this.lateAfterMinutes = lateAfterMinutes;
+    }
+
+    // Getters & Setters
+    public String getName() { 
+        return name; 
+    }
+
+    public void setName(String name) { 
+        this.name = name; 
+    }
+
+    public String getStartAt() { 
+        return startAt; 
+    }
+
+    public void setStartAt(String startAt) { 
+        this.startAt = startAt; 
+    }
+
+    public String getEndAt() { 
+        return endAt; 
+    }
+
+    public void setEndAt(String endAt) { 
+        this.endAt = endAt; 
+    }
+
+    public boolean isOpen() { 
+        return isOpen; 
+    }
+
+    public void setOpen(boolean open) { 
+        isOpen = open; 
+    }
+
+    public Integer getLateAfterMinutes() { 
+        return lateAfterMinutes; 
+    }
+
+    public void setLateAfterMinutes(Integer lateAfterMinutes) { 
+        this.lateAfterMinutes = lateAfterMinutes; 
+    }
+
+    public List<Attendance> getAttendances() { 
+        return attendances; 
+    }
+
+    public void setAttendances(List<Attendance> attendances) { 
+        this.attendances = attendances; 
+    }
+}

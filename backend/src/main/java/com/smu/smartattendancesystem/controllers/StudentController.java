0779d02db818a0a6480d7a60944ee0da929e169d
P2302676.Xavier Lim <2b03.2302676.xavierlim@gmail.com>
2025-09-24 09:51:57 +0800

    Updated test Student controller

--- backend/src/main/java/com/smu/smartattendancesystem/controllers/StudentController.java
+++ backend/src/main/java/com/smu/smartattendancesystem/controllers/StudentController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/students") // Changed to avoid conflicts
+@RequestMapping("/api/students") 
 public class StudentController {
     private final StudentManager studentManager;
 

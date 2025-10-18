bbc1854a2a06e1a71fd06fe0822741035e3ed9ee
2a22.2309110.lisheng <2a22.2309110.lisheng@gmail.com>
2025-10-18 19:00:46 +0800

    feat: add UpdateRosterButton component and integrate it into the Toolbar

--- frontend/src/pages/RosterView/Toolbar.jsx
+++ frontend/src/pages/RosterView/Toolbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AddStudentToRosterButton from './AddStudentToRosterButton';
+import UpdateRosterButton from './UpdateRosterbutton';
 import { Users } from 'lucide-react';
 
 const Toolbar = ({ roster }) => {
@@ -8,12 +9,7 @@ const Toolbar = ({ roster }) => {
       <div className="flex items-center justify-between bg-white/60 backdrop-blur-md border border-slate-200/60 shadow-sm rounded-2xl px-6 py-6 transition-all hover:shadow-md">
         {/* Left Section */}
         <div className="flex items-center gap-3">
-          <div className="p-2 bg-indigo-100 text-indigo-700 rounded-xl">
-            <Users className="w-6 h-6" strokeWidth={2.3} />
-          </div>
-          <div>
-            <h1 className="text-2xl font-bold text-gray-800 tracking-tight">Students</h1>
-          </div>
+          <UpdateRosterButton roster={roster} />
         </div>
 
         {/* Right Section */}

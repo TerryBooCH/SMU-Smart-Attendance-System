9adcd152e8a3280366d70e1d2359adebc07bf1ea
Sidjaz <sidneyyj@gmail.com>
2025-10-22 08:37:43 +0800

    Update SmartAttendanceSystemApplication.java

--- backend/src/main/java/com/smu/smartattendancesystem/SmartAttendanceSystemApplication.java
+++ backend/src/main/java/com/smu/smartattendancesystem/SmartAttendanceSystemApplication.java
@@ -1,7 +1,13 @@
 package com.smu.smartattendancesystem;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.smu.smartattendancesystem.models.User;
+import com.smu.smartattendancesystem.repositories.UserRepository;
 
 @SpringBootApplication
 public class SmartAttendanceSystemApplication {
@@ -10,4 +16,15 @@ public class SmartAttendanceSystemApplication {
 		SpringApplication.run(SmartAttendanceSystemApplication.class, args);
 	}
 
+	@Bean
+    CommandLineRunner initDatabase(UserRepository userRepo) {
+        return args -> {
+            if (userRepo.count() == 0) {
+				BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+                userRepo.save(new User("professor1", "prof1@gmail.com", encoder.encode("password123"), 2));
+                userRepo.save(new User("ta1", "ta1@gmail.com", encoder.encode("password123"), 0));
+                System.out.println("Default users successfully created in SQLite database.");
+            }
+        };
+    }
 }

38d684574dcd5f90dc821bc09e63337fbf719fbd
P2302676.Xavier Lim <2b03.2302676.xavierlim@gmail.com>
2025-10-26 02:43:38 +0800

    Added logging to BiometricController

--- backend/src/main/java/com/smu/smartattendancesystem/controllers/BiometricController.java
+++ backend/src/main/java/com/smu/smartattendancesystem/controllers/BiometricController.java
@@ -7,6 +7,7 @@ import java.util.*;
 import com.smu.smartattendancesystem.biometrics.detection.DetectionResult;
 
 import com.smu.smartattendancesystem.services.BiometricService;
+import com.smu.smartattendancesystem.utils.LoggerFacade;
 import com.smu.smartattendancesystem.dto.*;
 import static com.smu.smartattendancesystem.utils.ResponseFormatting.*;
 import org.springframework.web.bind.annotation.*;
@@ -27,11 +28,20 @@ public class BiometricController {
     public ResponseEntity<?> detect(@RequestParam("image") MultipartFile image, @RequestParam("type") String type) {
         try {
             List<DetectionResultDTO> results = this.biometricService.detect(image, type);
+            LoggerFacade.info("Biometric detection successful - Type: " + type + 
+                            ", File: " + image.getOriginalFilename() + 
+                            ", Results: " + results.size());
             return ResponseEntity.ok(results);
         } catch (IllegalArgumentException e) {
+            LoggerFacade.warning("Invalid biometric detection request - Type: " + type + 
+                            ", File: " + image.getOriginalFilename() + 
+                            " - " + e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(createErrorResponse(e.getMessage()));
         } catch (Exception e) {
+            LoggerFacade.severe("Unexpected error during biometric detection - Type: " + type + 
+                            ", File: " + image.getOriginalFilename() + 
+                            " - " + e.getMessage());
             System.out.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(createErrorResponse("An error occurred while detecting."));
